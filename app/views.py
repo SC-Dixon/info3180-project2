c3a260d1f387eae3206f43c5a98c47970a3103aa
Peter Thelwell <pthelwell55@gmail.com>
2023-04-12 20:01:07 -0500

    Created database

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, jsonify, send_file
+from .models import Posts, Likes, Follows, Users
 import os
 
 
