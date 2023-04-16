f25db34817b3f7158edf7d7ba3220d21cd78748d
SC-Dixon <shantay1234.sd@gmail.com>
2023-04-16 14:47:41 -0500

    Created views for first four routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
-csrf = CSRFProtect(app)
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
+
+#Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.config['WTF_CSRF_ENABLED'] = False
+
 
 
 
--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,24 @@
 # Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import InputRequired, Email
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    location = StringField('Location', validators=[InputRequired()])
+    biography = TextAreaField('Biography', validators=[InputRequired()])
+    profile_photo = TextAreaField('photo', validators=[InputRequired()])
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+class PostForm(FlaskForm):
+    photo = TextAreaField('Photo', validators=[InputRequired()])
+    caption = TextAreaField('Caption', validators=[InputRequired()])
+    
